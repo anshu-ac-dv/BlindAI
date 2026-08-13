d7ad7c316deb47a08f59133ade1c4dd2136b1e36
Anshu Kumar <anshu.ac.dv@gmail.com>
2026-08-13 21:59:38 +0530

    designing header

--- blind_ai/src/assets/components/header.jsx
+++ blind_ai/src/assets/components/header.jsx
@@ -4,12 +4,13 @@ export default function Header() {
   return (
     <header className="header">
       <a href="#" className="logo">
-        MyViteApp
+        BlindAI
       </a>
       
       <nav>
         <ul className="nav-list">
           <li><a href="#home" className="nav-link">Home</a></li>
+          <li><a href="#services" className="nav-link">Services</a></li>
           <li><a href="#features" className="nav-link">Features</a></li>
           <li><a href="#about" className="nav-link">About</a></li>
         </ul>
